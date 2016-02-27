47a0d508a8ec7bdc39cfeb26ce813db102333997
wolfemm <mike.wolfe.millard@gmail.com>
2016-02-26 19:31:10 -0800

    Downcase fetched domains

--- pull_mailchecker_emails.rb
+++ pull_mailchecker_emails.rb
@@ -9,8 +9,6 @@ whitelisted_domains = %w(poczta.onet.pl fastmail.fm hushmail.com naver.com)
 
 existing_domains = File.readlines("vendor/disposable_domains.txt")
 
-puts existing_domains.size
-
 url = "https://raw.githubusercontent.com/FGRibreau/mailchecker/master/list.json"
 resp = Net::HTTP.get_response(URI.parse(url))
 
@@ -18,6 +16,6 @@ remote_domains = JSON.parse(resp.body).flatten - whitelisted_domains
 
 puts "New domains found: #{(remote_domains - existing_domains).join(', ')}"
 
-result_domains = (existing_domains + remote_domains).map(&:strip).uniq.sort
+result_domains = (existing_domains + remote_domains).map { |domain| domain.strip.downcase }.uniq.sort
 
 File.open("vendor/disposable_domains.txt", "w") { |f| f.write result_domains.join("\n") }
