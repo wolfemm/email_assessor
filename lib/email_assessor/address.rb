a47c9fd9e7c6d16c799d2b4af5ee889c4035cda5
wolfemm <mike.wolfe.millard@gmail.com>
2017-04-30 00:21:03 -0700

    Use \Z rather than \z in regex to avoid unintended behavior

--- lib/email_assessor/address.rb
+++ lib/email_assessor/address.rb
@@ -26,7 +26,7 @@ module EmailAssessor
         domain.match?(%r{\.}) && # Valid address domain must contain a period
           !domain.match?(%r{\.{2,}}) && # Valid address domain cannot have consecutive periods
           !domain.match?(%r{\A\.}) && # Valid address domain cannot start with a period
-          domain.match?(%r{[a-z]\Z}i) # Valid address domain must end with letters
+          domain.match?(%r{[a-z]\z}i) # Valid address domain must end with letters
       else
         false
       end
