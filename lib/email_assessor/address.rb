41afbda1d8ee3d5ce46c6e06207c3c9c9b86f671
wolfemm <mike.wolfe.millard@gmail.com>
2017-04-30 00:19:52 -0700

    Use \A rather than ^ in regex to avoid unintended behavior

--- lib/email_assessor/address.rb
+++ lib/email_assessor/address.rb
@@ -25,7 +25,7 @@ module EmailAssessor
 
         domain.match?(%r{\.}) && # Valid address domain must contain a period
           !domain.match?(%r{\.{2,}}) && # Valid address domain cannot have consecutive periods
-          !domain.match?(%r{^\.}) && # Valid address domain cannot start with a period
+          !domain.match?(%r{\A\.}) && # Valid address domain cannot start with a period
           domain.match?(%r{[a-z]\Z}i) # Valid address domain must end with letters
       else
         false
