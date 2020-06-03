e74577625794dfe0f15d1e52caa42c1346be5666
wolfemm <mike.wolfe.millard@gmail.com>
2020-06-03 03:41:03 -0700

    Pass the chomp flag to File.foreach instead of chomping manually

--- lib/email_assessor.rb
+++ lib/email_assessor.rb
@@ -17,9 +17,7 @@ module EmailAssessor
     file_name ||= ""
     domain = domain.downcase
 
-    File.foreach(file_name).any? do |line|
-      line = line.chomp
-
+    File.foreach(file_name, chomp: true).any? do |line|
       # String#end_with? is used as a cheaper initial check but due to potential false positives
       # (hotmail.com is valid but tmail.com is not) regex is also necessary.
       domain.end_with?(line) && domain.match?(%r{\A(?:.*\.)?#{line}\z}i)
