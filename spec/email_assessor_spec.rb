593e512cd05657938bc6bfa9848f73f9f8120423
wolfemm <mike.wolfe.millard@gmail.com>
2020-06-03 03:31:35 -0700

    Tidy up specs

--- spec/email_assessor_spec.rb
+++ spec/email_assessor_spec.rb
@@ -32,32 +32,32 @@ describe EmailAssessor do
   describe "basic validation" do
     subject(:user) { plain_user }
 
-    it "should be valid when email is empty" do
+    it "is valid when email is empty" do
       is_expected.to be_valid
     end
 
-    it "should not be valid when domain is missing" do
+    it "is invalid when domain is missing" do
       user.email = "foo@.com"
       is_expected.to be_invalid
     end
 
-    it "should be invalid when email is malformed" do
+    it "is invalid when email is malformed" do
       user.email = "foo@bar"
       is_expected.to be_invalid
     end
 
-    it "should be invalid when email contains a trailing symbol" do
+    it "is invalid when email contains a trailing symbol" do
       user.email = "foo@bar.com/"
       is_expected.to be_invalid
     end
 
-    it "should be invalid if Mail::AddressListsParser raises exception" do
+    it "is invalid if Mail::AddressListsParser raises exception" do
       allow(Mail::Address).to receive(:new).and_raise(Mail::Field::ParseError.new(nil, nil, nil))
       user.email = "foo@gmail.com"
       is_expected.to be_invalid
     end
 
-    it "shouldn't be valid if the domain constains consecutives dots" do
+    it "is invalid if the domain constains consecutives dots" do
       user.email = "foo@bar..com"
       is_expected.to be_invalid
     end
@@ -72,6 +72,21 @@ describe EmailAssessor do
       is_expected.to be_invalid
     end
 
+    it "is invalid if the domain contains '.@'" do
+      user.email = "foo.@gmail.com"
+      expect(user.valid?).to be_falsy
+    end
+
+    it "is invalid if the domain begins with a hyphen" do
+      user.email = "foo@-gmail.com"
+      expect(user.valid?).to be_falsy
+    end
+
+    it "is invalid if the domain name ends with a hyphen" do
+      user.email = "foo@gmail-.com"
+      expect(user.valid?).to be_falsy
+    end
+
     %w[+ _ ! / \  '].each do |invalid_character|
       it "is invalid if domain contains a \"#{invalid_character}\" character" do
         user.email = "foo@google#{invalid_character}yahoo.com"
@@ -83,59 +98,44 @@ describe EmailAssessor do
   describe "disposable domains" do
     subject(:user) { disposable_user }
 
-    it "should be valid when email is not in the list of disposable domains" do
+    it "is valid when email is not in the list of disposable domains" do
       is_expected.to be_valid
     end
 
-    it "should be invalid when email is in the list of disposable domains" do
+    it "is invalid when email is in the list of disposable domains" do
       user.email = "foo@#{disposable_domain}"
       is_expected.to be_invalid
     end
 
-    it "should be invalid when email is in the list of disposable domains regardless of case" do
+    it "is invalid when email is in the list of disposable domains regardless of case" do
       user.email = "foo@#{disposable_domain.upcase}"
       is_expected.to be_invalid
     end
 
-    it "should be invalid when email is in the list of disposable domains regardless of subdomain" do
+    it "is invalid when email is in the list of disposable domains regardless of subdomain" do
       user.email = "foo@abc123.#{disposable_domain}"
       is_expected.to be_invalid
-    end
-
-    it "is invalid if the domain contains '.@'" do
-      user.email = "foo.@gmail.com"
-      expect(user.valid?).to be_falsy
-    end
-
-    it "is invalid if the domain begins with a hyphen" do
-      user = TestUser.new(email: "foo@-gmail.com")
-      expect(user.valid?).to be_falsy
-    end
-
-    it "is invalid if the domain name ends with a hyphen" do
-      user = TestUser.new(email: "foo@gmail-.com")
-      expect(user.valid?).to be_falsy
     end
   end
 
   describe "blacklisted domains" do
     subject(:user) { blacklist_user }
 
-    it "should be valid when email domain is not in the blacklist" do
+    it "is valid when email domain is not in the blacklist" do
       is_expected.to be_valid
     end
 
-    it "should be invalid when email domain is in the blacklist" do
+    it "is invalid when email domain is in the blacklist" do
       user.email = "foo@#{blacklisted_domain}"
       is_expected.to be_invalid
     end
 
-    it "should be invalid when email is in the blacklist regardless of case" do
+    it "is invalid when email is in the blacklist regardless of case" do
       user.email = "foo@#{blacklisted_domain.upcase}"
       is_expected.to be_invalid
     end
 
-    it "should be invalid when email domain is in the blacklist regardless of subdomain" do
+    it "is invalid when email domain is in the blacklist regardless of subdomain" do
       user.email = "foo@abc123.#{blacklisted_domain}"
       is_expected.to be_invalid
     end
@@ -144,11 +144,11 @@ describe EmailAssessor do
   describe "mx lookup" do
     subject(:user) { mx_user }
 
-    it "should be valid if mx records are found" do
+    it "is valid if mx records are found" do
       is_expected.to be_valid
     end
 
-    it "should be invalid if no mx records are found" do
+    it "is invalid if no mx records are found" do
       user.email = "foo@subdomain.gmail.com"
       is_expected.to be_invalid
     end
