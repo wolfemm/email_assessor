260a2c4efe000973a11770c302cc80e19564f2e4
wolfemm <mike.wolfe.millard@gmail.com>
2024-08-27 11:16:20 -0700

    Improve pull_mailchecker_emails.rb
    - Pull from an additional master list
    - Improve exclusion of domains from the disposable list
    - Run a reindex after updating the list file

--- pull_mailchecker_emails.rb
+++ pull_mailchecker_emails.rb
@@ -6,18 +6,26 @@ require "yaml"
 require "json"
 require "net/http"
 require "set"
+require_relative "lib/email_assessor/file_domain_list"
+require_relative "lib/email_assessor/domain_token_set"
 
-whitelisted_domains = %w(poczta.onet.pl fastmail.fm hushmail.com naver.com qq.com nus.edu.sg)
-
+excluded_domains = EmailAssessor::FileDomainList.new("src/excluded_domains.txt")
 existing_domains = File.readlines("src/disposable_domains.txt")
 
-url = "https://raw.githubusercontent.com/FGRibreau/mailchecker/master/list.txt"
-resp = Net::HTTP.get_response(URI.parse(url))
+remote_domains = [
+  "https://raw.githubusercontent.com/FGRibreau/mailchecker/master/list.txt",
+  "https://raw.githubusercontent.com/disposable/disposable-email-domains/master/domains.txt"
+].flat_map do |url|
+  resp = Net::HTTP.get_response(URI.parse(url))
 
-remote_domains = (resp.body.split("\n")) - whitelisted_domains
+  resp.body.split("\n").flatten
+end
 
 result_domains = Set.new((existing_domains + remote_domains).map! { |domain| domain.strip.downcase })
 result_domains = result_domains.to_a
+result_domains.delete_if { |domain| excluded_domains.include_any?(EmailAssessor::DomainTokenSet.parse(domain)) }
 result_domains.sort!
 
 File.open("src/disposable_domains.txt", "w") { |f| f.write result_domains.join("\n") }
+
+require_relative "index_domains"
